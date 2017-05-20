7a463643e763fd853da92b5649c5ff3886a3af15
cl <cl_fighting@163.com>
2017-05-20 17:04:26 +0800

    empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,11 @@ class CartsController < ApplicationController
   def index
     @carts = Cart.all
   end
+
+  def clean
+    current_cart.clean!
+    flash[:warning] = "已清空购物车"
+    redirect_to carts_path
+  end
+
 end
