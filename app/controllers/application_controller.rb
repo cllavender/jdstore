4286889c58caf8fc0b6c4bb9b8444a8b41729e3e
cl <cl_fighting@163.com>
2017-05-20 09:19:14 +0800

    adjust details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def require_admin
-    if !current_user.is_admin
+    if !current_user.admin?
       flash[:warning] = "You are not admin"
       redirect_to "/"
     end
